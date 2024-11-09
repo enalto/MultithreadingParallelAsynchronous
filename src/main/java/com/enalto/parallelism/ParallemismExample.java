2a9fb020f317825e2db40f8528cdb4568d6a4e86
enalto <enaltogondrige@gmail.com>
2024-11-09 19:02:48 -0300

    Added class ParallemismExample

--- src/main/java/com/enalto/parallelism/ParallemismExample.java
+++ src/main/java/com/enalto/parallelism/ParallemismExample.java
@@ -1,4 +1,26 @@
 package com.enalto.parallelism;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class ParallemismExample {
+
+    public static void main(String[] args) {
+
+        List<String> namesList = Arrays.asList("enalto", "juliana", "giovanna", "vitoria", "enaldinho");
+
+        List<String> list = namesList.stream()
+                .parallel()
+                .map(String::toUpperCase)
+                .collect(Collectors.toList());
+
+        list.forEach(System.out::println);
+
+        for(String listItem: list){
+            System.out.println(listItem);
+        }
+    }
+
+
 }
