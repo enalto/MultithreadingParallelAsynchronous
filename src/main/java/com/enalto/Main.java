848e1c0ed1b22c40c3b220d2334f99ead065d272
enalto <enaltogondrige@gmail.com>
2024-11-10 20:16:50 -0300

    Added class ProductServiceUsingExecutorService

--- src/main/java/com/enalto/Main.java
+++ src/main/java/com/enalto/Main.java
@@ -1,21 +1,29 @@
 package com.enalto;
 
 import com.enalto.domain.Product;
+import com.enalto.executor.ProductServiceUsingExecutorService;
 import com.enalto.service.ProductInfoService;
 import com.enalto.service.ReviewService;
 import com.enalto.thread.ProductServiceUsingThread;
 
+import java.util.concurrent.ExecutionException;
+
 public class Main {
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) throws InterruptedException, ExecutionException {
 
         ProductInfoService productInfoService = new ProductInfoService();
         ReviewService reviewService = new ReviewService();
         //ProductService productService = new ProductService(productInfoService, reviewService);
-        ProductServiceUsingThread productService =
-                new ProductServiceUsingThread(productInfoService, reviewService);
+//        ProductServiceUsingThread productService =
+//                new ProductServiceUsingThread(productInfoService, reviewService);
+
+        ProductServiceUsingExecutorService productService =
+                new ProductServiceUsingExecutorService(productInfoService, reviewService);
+
 
         String productId = "ABCD";
         Product product = productService.retrieveProductDetails(productId);
         System.out.println(product);
+        System.out.println("availableProcessors= " + Runtime.getRuntime().availableProcessors());
     }
 }
---
+++ src/main/java/com/enalto/executor/ProductServiceUsingExecutorService.java
@@ -0,0 +1,81 @@
+package com.enalto.executor;
+
+import com.enalto.domain.Product;
+import com.enalto.domain.ProductInfo;
+import com.enalto.domain.Review;
+import com.enalto.service.ProductInfoService;
+import com.enalto.service.ReviewService;
+
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+import static com.enalto.util.CommonUtil.stopWatch;
+
+public class ProductServiceUsingExecutorService {
+    private final ExecutorService executorService =
+            Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
+
+    private final ProductInfoService productInfoService;
+    private final ReviewService reviewService;
+
+    public ProductServiceUsingExecutorService(final ProductInfoService productInfoService, final ReviewService reviewService) {
+        this.productInfoService = productInfoService;
+        this.reviewService = reviewService;
+    }
+
+    public Product retrieveProductDetails(String productId) throws InterruptedException, ExecutionException {
+        stopWatch.start();
+
+        Future<ProductInfo> productInfoFuture = executorService
+                .submit(() -> productInfoService.retrieveProductInfo(productId));
+
+        Future<Review> reviewFuture = executorService
+                .submit(() -> reviewService.retrieveReviews(productId));
+
+
+        ProductInfo productInfo = productInfoFuture.get();
+        Review review = reviewFuture.get();
+
+        stopWatch.stop();
+        System.out.println(stopWatch.getElapsedTime());
+        return new Product(productId, productInfo, review);
+    }
+
+    private class ProductInfoRunnable implements Runnable {
+        private final String productId;
+        private ProductInfo productInfo;
+
+        public ProductInfo getProductInfo() {
+            return productInfo;
+        }
+
+        public ProductInfoRunnable(String productId) {
+            this.productId = productId;
+        }
+
+        public void run() {
+            this.productInfo = productInfoService.retrieveProductInfo(productId);
+        }
+    }
+
+    private class ReviewRunnable implements Runnable {
+        private final String productId;
+        private Review review;
+
+
+        public Review getReview() {
+            return review;
+        }
+
+        public ReviewRunnable(String productId) {
+            this.productId = productId;
+        }
+
+        @Override
+        public void run() {
+            this.review = reviewService.retrieveReviews(productId);
+        }
+    }
+}
