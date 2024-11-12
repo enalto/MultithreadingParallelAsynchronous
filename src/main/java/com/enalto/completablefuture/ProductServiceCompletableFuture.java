e2c9fb24055e497e2c760f09708ed92231bec1e2
enalto <enaltogondrige@gmail.com>
2024-11-12 09:43:33 -0300

    Added class ProductServiceCompletableFuture. Implement completableFuture strategy.

--- src/main/java/com/enalto/completablefuture/ProductServiceCompletableFuture.java
+++ src/main/java/com/enalto/completablefuture/ProductServiceCompletableFuture.java
@@ -1,19 +1,21 @@
 package com.enalto.completablefuture;
 
-import com.enalto.domain.Product;
-import com.enalto.domain.ProductInfo;
-import com.enalto.domain.Review;
+import com.enalto.domain.*;
+import com.enalto.service.InvetoryService;
 import com.enalto.service.ProductInfoService;
 import com.enalto.service.ReviewService;
 
+import java.util.List;
 import java.util.Objects;
 import java.util.concurrent.CompletableFuture;
+import java.util.stream.Collectors;
 
 import static com.enalto.util.CommonUtil.stopWatch;
 
 public class ProductServiceCompletableFuture {
     private final ProductInfoService productInfoService;
     private final ReviewService reviewService;
+    private InvetoryService invetoryService;
 
     public ProductServiceCompletableFuture(final ProductInfoService productInfoService, final ReviewService reviewService) {
         Objects.requireNonNull(productInfoService);
@@ -21,6 +23,13 @@ public class ProductServiceCompletableFuture {
 
         this.productInfoService = productInfoService;
         this.reviewService = reviewService;
+    }
+
+    public ProductServiceCompletableFuture(ProductInfoService productInfoService,
+                                           ReviewService reviewService, InvetoryService invetoryService) {
+        this.productInfoService = productInfoService;
+        this.reviewService = reviewService;
+        this.invetoryService = invetoryService;
     }
 
     public Product retrieveProductDetails(String productId) {
@@ -42,6 +51,39 @@ public class ProductServiceCompletableFuture {
         stopWatch.stop();
         System.out.println(stopWatch.getElapsedTime());
         return product;
+    }
+
+    public Product retrieveProductDetailsWithInventory(String productId) {
+        stopWatch.start();
+
+        CompletableFuture<ProductInfo> productInfoCompletableFuture =
+                CompletableFuture.supplyAsync(() -> productInfoService.retrieveProductInfo(productId))
+                        .thenApply(productInfo -> {
+                            List<ProductOption> productOptions = updateInventory(productInfo);
+                            productInfo.setProductOptions(productOptions);
+                            return productInfo;
+                        });
+
+        CompletableFuture<Review> reviewCompletableFuture =
+                CompletableFuture.supplyAsync(() -> reviewService.retrieveReviews(productId));
+
+        Product product = productInfoCompletableFuture.thenCombine(reviewCompletableFuture,
+                (productInfo, review) -> new Product(productId, productInfo, review)
+        ).join();
+
+        stopWatch.stop();
+        System.out.println(stopWatch.getElapsedTime());
+        return product;
+    }
+
+    private List<ProductOption> updateInventory(ProductInfo productInfo) {
+        return productInfo.getProductOptions()
+                .stream()
+                .map((productOption) -> {
+                    Inventory inventory = invetoryService.retrieveInventory(productOption);
+                    productOption.setInventory(inventory);
+                    return productOption;
+                }).collect(Collectors.toList());
     }
 
     public CompletableFuture<Product> retrieveProductDetailsNonBlocking(String productId) {
